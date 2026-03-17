a5784d0b5bf4cb9f6a6e3c3b2f4e73f871571836
Ayush <gwayush444@gmail.com>
2026-03-17 11:51:25 +0545

    fix the drizlle

--- src/lib/server/db/index.js
+++ src/lib/server/db/index.js
@@ -1,5 +1,4 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-// import Database from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client'
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
